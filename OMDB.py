c2e470705bba44553bbcf448fe27a14827e0514d
Arunkumar Shibu <86429222+Arun-TG@users.noreply.github.com>
2022-01-09 23:31:47 +0530

    Update OMDB.py

--- OMDB.py
+++ OMDB.py
@@ -1,34 +1,34 @@
 #sheriyenna
 
-import os
 import requests
 from info import API_KEY
 
-def get_movie_info(movieTitle):
-    url = 'http://www.omdbapi.com'
-    api_key = API_KEY
-    data = {'apikey':api_key,'t':movieTitle}
-    response = requests.get(url,data).json()
+user = {"User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57"}
 
-    if response.get("Response") != "True":
-        return None
+def get_movie_info(query):    
+    try:
+       url = f'http://www.omdbapi.com/?apikey={API_KEY}&t={query}'
+       resp = requests.get(url, headers=user).json()
+       poster=resp['Poster']
+       id=resp['imdbID']
+       text=f"""📀 𝖳𝗂𝗍𝗅𝖾 : <b><u>{resp['Title']}</u></b>
+                            
+⏱️ 𝖱𝗎𝗇𝗍𝗂𝗆𝖾 : <b>{resp['Runtime']}</b>
+🌟 𝖱𝖺𝗍𝗂𝗇𝗀 : <b>{resp['imdbRating']}/10</b>
+🗳️ 𝖵𝗈𝗍𝖾𝗌 : <b>{resp['imdbVotes']}</b>
 
-    movie_info = {}
-    movie_info["title"] = response.get("Title")
-    movie_info["year"] = response.get("Year")
-    movie_info["release"] = response.get("Released")
-    movie_info["duration"] = response.get("Runtime")
-    movie_info["genre"] = response.get("Genre")
-    movie_info["director"] = response.get("Director")
-    movie_info["writer"] = response.get("Writer")
-    movie_info["actors"] = response.get("Actors")
-    movie_info["plot"] = response.get("Plot")
-    movie_info["language"] = response.get("Language")
-    movie_info["country"] = response.get("Country")
-    movie_info["ratings"] = response.get("Ratings")
-    movie_info["imdb_rating"] = response.get("imdbRating")
-    movie_info["votes"] = response.get("imdbVotes")
-    movie_info["imdb_id"] = response.get("imdbID")
-    movie_info["pimage"] = response.get("Poster")
+📆 𝖱𝖾𝗅𝖾𝖺𝗌𝖾 : <b>{resp['Released']}</b>
+🎭 𝖦𝖾𝗇𝗋𝖾 : <b>{resp['Genre']}</b>
+🎙 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : <b>{resp['Language']}</b>
+🌐 𝖢𝗈𝗎𝗇𝗍𝗋𝗒 : <b>{resp['Country']}</b>
 
-    return movie_info
+🎥 𝖣𝗂𝗋𝖾𝖼𝗍𝗈𝗋𝗌 : <b>{resp['Director']}</b>
+📝 𝖶𝗋𝗂𝗍𝖾𝗋𝗌 : <b>{resp['Writer']}</b>
+🔆 𝖲𝗍𝖺𝗋𝗌 : <b>{resp['Actors']}</b>
+
+🗒 𝖯𝗅𝗈𝗍 : <code>{resp['Plot']}</code>"""
+
+    except Exception as error:
+        print(error)
+    return poster, id, text
+         
